4923ad59eaf3f18512780e3b836ceeb665d56e92
HassanAbdulgad <hassanabdulgader16@gmail.com>
2023-10-17 15:38:19 -0700

    Part of printf function

--- main.h
+++ main.h
@@ -84,11 +84,9 @@ int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* width handler */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
